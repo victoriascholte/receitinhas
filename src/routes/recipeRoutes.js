e44585f38e25fb275100ec936e7f51afdf79360f
Victoria Scholte <victoria.scholte@gmail.com>
2022-07-11 23:08:25 -0300

    feat/vs-add-recipe-controller-and-routes-initial-files

---
+++ src/routes/recipeRoutes.js
@@ -0,0 +1,12 @@
+const controller = require('../controller/recipeController')
+const express = require('express')
+
+const router = express.Router()
+
+//router.post("/recipes", controller.createRecipes)
+//router.get("/recipes", controller.findAllRecipes)
+//router.get("/recipes/:id", controller.findRecipeById)
+//router.patch("/recipes/:id", controller.updateRecipe)
+//router.delete("/recipes/:id", controller.deleteRecipe)
+
+module.exports = router
