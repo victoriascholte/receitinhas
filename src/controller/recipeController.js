9cc773f6f0c74fe4f3ed3b0751b39684758487f4
Victoria Scholte <victoria.scholte@gmail.com>
2022-07-12 12:13:36 -0300

    Feature/vs add routes and controller to recipes endpoints (#4)
    * fix/vs-update-gitignore-and-dotenv
    
    * feat/vs-add-recipe-controller-and-routes-initial-files
    
    * fix/vs-update-recipeModel.js
    
    * fix/vs-update-gitignore
    
    * fix/vs-remove-duplicated-file
    
    * fix/mongodb-security-adjustment

---
+++ src/controller/recipeController.js
@@ -0,0 +1,22 @@
+const RecipeModel = require('../models/recipeModel')
+const SECRET = process.env.SECRET
+
+// nova Receita
+// const createRecipe 
+
+// listar Receitas
+// const findAllRecipes
+
+// buscar por Id
+// findRecipeById
+
+// atualizar Receita
+// updateRecipe
+
+// deletar Receita
+// deleteRecipe
+
+
+// module.exports = {
+//     createRecipe, findAllRecipes, findRecipeById, updateRecipe, deleteRecipe
+// }
