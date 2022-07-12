defb3fcee6c3b840bc0704b2401a4cbb5363900a
Victoria Scholte <victoria.scholte@gmail.com>
2022-07-12 12:38:31 -0300

    fix/update-gitignore-and-app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('./database/mongooseConnect')
+const mongoose = require('./src/database/mongooseConnect')
 
 console.log("Hello world!")
 
