1755cd07df8a1cbb7b39db527055e631417d3a51
Victoria Scholte <victoria.scholte@gmail.com>
2022-07-16 15:37:43 -0300

    feat: add index and update controllers and routes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('./src/database/mongooseConnect')
+const index = require('./src/routes/index')
 
 console.log("Hello world!")
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use('/', index)
 mongoose.connect()
 
 
--- src/controller/recipeController.js
+++ src/controller/recipeController.js
@@ -2,27 +2,21 @@ const RecipeModel = require('../models/recipeModel')
 const SECRET = process.env.SECRET
 
 //// NOVA RECEITA
-
 // const createRecipe
 
 //// TODAS AS RECEITAS 
-
 // const allRecipes 
+
+//// BUSCAR POR ID
+// findRecipeById
+
+// ATUALIZAR RECEITA
+// updateRecipe
+
+// DELETAR RECEITA
+// deleteRecipe
 
 
 // module.exports = {
-  
+
 // }
-
-
-// listar Receitas
-// const allRecipes
-
-// buscar por Id
-// findRecipeById
-
-// atualizar Receita
-// updateRecipe
-
-// deletar Receita
-// deleteRecipe
