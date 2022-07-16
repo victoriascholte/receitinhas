03925c68f9be4ad51a740db6096b676a9d65178f
Victoria Scholte <victoria.scholte@gmail.com>
2022-07-16 15:42:55 -0300

    fix: update swagger in app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/', index)
-app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 mongoose.connect()
 
