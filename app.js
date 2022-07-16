ee99b5ccd2a65002c67f83ceb655c190d888b21c
Victoria Scholte <victoria.scholte@gmail.com>
2022-07-16 15:42:37 -0300

    fix: update swagger in app

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const cors = require('cors')
 const mongoose = require('./src/database/mongooseConnect')
 const index = require('./src/routes/index')
 
-console.log("Hello world!")
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/', index)
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 mongoose.connect()
 
-
 module.exports = app
+
+console.log("Estamos aqui...")
